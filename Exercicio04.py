24eefe298b04aac5fb46617940cade1bfdd7ac1a
Gustavo Souza <gggutavo1301@gmail.com>
2025-04-10 09:44:24 -0300

    Sistama de repetição e se o número for negativo msotra a quantidade

--- Exercicio04.py
+++ Exercicio04.py
@@ -2,5 +2,5 @@ num = int(input("Digite um número: "))
 if num <= 0:
     print("Número inválido")
 else:
-    for N in range(1, num + 1,1):
+    for N in range(1, num + 1, 1):
         print(N, end= " ")
---
+++ Exercicio05.py
@@ -0,0 +1,6 @@
+negat = 0
+for valor in range(1,5):
+    num = int(input("Digite um número: "))
+    if num <0:
+        negat += 1
+print(negat)
